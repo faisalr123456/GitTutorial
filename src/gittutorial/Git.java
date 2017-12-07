39768fb780cdcc30fcd82726584134a443b38337
faisalr123456 <tuc66840@temple.edu>
2017-12-06 19:53:51 -0800

    added a lot of syso

--- src/gittutorial/Git.java
+++ src/gittutorial/Git.java
@@ -3,6 +3,8 @@ package gittutorial;
 public class Git {
 
     public static void main(String[] args) {
+        System.out.print("Git is cool");System.out.print("Git is cool");
+        System.out.print("Git is cool");
         System.out.print("Git is cool");
     }
 
